8bbf50f7d67261c1658b561dcc7022c34df19c1b
xYairrx <xyaireag@gmail.com>
2022-06-05 21:02:29 -0500

    Ejercicio examen

---
+++ src/empleado/Empleado.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package empleado;
+
+/**
+ *
+ * @author Yair
+ */
+public class Empleado {
+    
+    private String nombre;
+    private String apellido;
+    private double salarioMensual;
+
+    public Empleado(String nombre, String apellido, double salarioMensual) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.salarioMensual = salarioMensual;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public double getSalarioMensual() {
+        return salarioMensual;
+    }
+
+    public void setSalarioMensual(double salarioMensual) {
+        this.salarioMensual = salarioMensual;
+    }
+    
+    public double aumentoSalario(){
+        double aumentoSalario = salarioMensual * 0.10;
+        return aumentoSalario;
+    }
+    
+}
